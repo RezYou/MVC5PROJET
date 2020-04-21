7eb5c4727c3e8683f4faf0ed0be8ec007916eda6
RezzYou <44924425+RezYou@users.noreply.github.com>
2020-04-21 20:03:39 +0000

    21/04/2020 20:03

--- StorePfm/Models/IdentityModels.cs
+++ StorePfm/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace StorePfm.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("PFMSTOREEntities", throwIfV1Schema: false)
         {
         }
 
